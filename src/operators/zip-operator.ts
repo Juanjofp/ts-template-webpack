dbf21f710ebea4c0838e3473be1624fc3627f626
juanjo <juanjo@juanjofp.com>
2023-07-03 18:28:57 +0200

    Add tips

--- src/operators/zip-operator.ts
+++ src/operators/zip-operator.ts
@@ -1,4 +1,4 @@
-import { from, zip, interval } from 'rxjs';
+import { from, zip, interval, map } from 'rxjs';
 
 console.log('Zip operator');
 
@@ -6,13 +6,13 @@ const syncObs$ = from([1, 2, 3, 4, 5]);
 
 const asyncObs$ = interval(1000);
 
-// const result$ = zip(syncObs$, asyncObs$).pipe(map(([val]) => val));
+const result$ = zip(syncObs$, asyncObs$).pipe(map(([val]) => val));
 
 // const result$ = zip([syncObs$, asyncObs$]).pipe(map(([val]) => val));
 
 // const result$ = zip([syncObs$, asyncObs$], (x, _) => x);
 
-const result$ = zip([syncObs$, asyncObs$], (...values) => values[0]);
+// const result$ = zip([syncObs$, asyncObs$], (...values) => values[0]);
 
 result$.subscribe({
   next: val => console.log('Emit', val),
